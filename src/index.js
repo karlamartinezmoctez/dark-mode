b8ea4f05a038f4edda334005f69c35d789fb5c37
karlamartinezmoctez <karlamartinezmoctez@flpoly.org>
2020-09-10 00:15:07 -0400

    mvp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-//comment
+import useDarkMode from "./hooks/useDarkMode"
+
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
@@ -10,6 +11,8 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
@@ -20,8 +23,8 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
   return (
-    <div className={darkmode ? "dark-mode App" : "App"}>
-      <Navbar />
+    <div className={darkMode ? "dark-mode App" : "App"}>
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode}/>
       <Charts coinData={coinData} />
     </div>
   );
